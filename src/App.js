93ee0c7018e2064022e5735047843b6fda6c6925
Eliezer <elimasyrubi@gmail.com>
2020-11-10 14:50:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from "react";
+import Header from "./Componentes/Header";
+import Formulario from "./Componentes/Formulario";
+import Clima from "./Componentes/Clima";
+import Error from "./Componentes/Error";
 
 function App() {
+  const [busqueda, guardarbusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
+  const [error, guardarError] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (consultar) {
+        const appId = "e802ba1a98c59e1b7fdd1a739ee4cb6e";
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        guardarResultado(resultado);
+        guardarConsultar(false);
+
+        // Detecta si hubo resultados correctos en la consulta
+        if (resultado.cod === "404") {
+          guardarError(true);
+        } else {
+          guardarError(false);
+        }
+      }
+    };
+
+    consultarAPI();
+  }, [consultar]);
+
+  let componente;
+  if (error) {
+    componente = <Error mensaje="No hay resultados" />;
+  } else {
+    componente = <Clima resultado={resultado} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header titulo="Clima React" />
+
+      <div className="contenedor-form">
+        <div className="contaienr">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario
+                busqueda={busqueda}
+                guardarbusqueda={guardarbusqueda}
+                guardarConsultar={guardarConsultar}
+              />
+            </div>
+            <div className="col m6 s12">{componente}</div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
